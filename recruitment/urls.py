b31604e38857840ea9a935da676c2986049d8d7a
Uma-1014 <umamaheswarir448@gmail.com>
2026-09-11 21:54:12 +0530

    Prepare application for Render deployment

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -1,48 +1,28 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-
     path(
-        'admin/',
-        admin.site.urls
+        "",
+        TemplateView.as_view(template_name="home.html"),
+        name="home",
     ),
 
-    path(
-        'accounts/',
-        include('accounts.urls')
-    ),
-
-    path(
-        'candidate/',
-        include('candidates.urls')
-    ),
-
-    path(
-        'jobs/',
-        include('jobs.urls')
-    ),
-
-    path(
-        'applications/',
-        include('applications.urls')
-    ),
-
-    path(
-        'ai/',
-        include('ai_engine.urls')
-    ),
-
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("candidate/", include("candidates.urls")),
+    path("jobs/", include("jobs.urls")),
+    path("applications/", include("applications.urls")),
+    path("ai/", include("ai_engine.urls")),
 ]
 
 
 if settings.DEBUG:
-
     urlpatterns += static(
         settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
+        document_root=settings.MEDIA_ROOT,
     )
